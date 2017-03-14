a44f3cecb091149db176917d07485a65d091d265
Jones Silva <jones.jssantos@gmail.com>
2017-03-13 21:32:21 -0300

    Initial Commit

---
+++ src/main/java/br/com/alura/listavip/confs/Configurador.java
@@ -0,0 +1,12 @@
+package br.com.alura.listavip.confs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configurador {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configurador.class, args);
+	}
+}
