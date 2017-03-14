9222843b08792a303a9ac029d466f17f66f3058a
Jones Silva <jones.jssantos@gmail.com>
2017-03-14 20:33:45 -0300

    Aula 03 - Salvando convidados

--- src/main/java/br/com/alura/listavip/controllers/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/controllers/ConvidadoController.java
@@ -1,13 +1,39 @@
 package br.com.alura.listavip.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.alura.listavip.models.Convidado;
+import br.com.alura.listavip.repositorys.ConvidadoRepository;
 
 @Controller
 public class ConvidadoController {
+	
+	@Autowired
+	private ConvidadoRepository repository;
 
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@RequestMapping("listaconvidados")
+	public String listaConvidados(Model model) {
+		Iterable<Convidado> convidados = repository.findAll();
+	    model.addAttribute("convidados", convidados);
+	    
+	    return "listaconvidados";
+	}
+
+	@RequestMapping(value="salvar", method=RequestMethod.POST)
+	public ModelAndView salvar(Convidado convidado) {
+		ModelAndView modelAndView = new ModelAndView("redirect:listaconvidados");
+		
+		repository.save(convidado);
+		return modelAndView;
 	}
 }
---
+++ src/main/java/br/com/alura/listavip/repositorys/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.alura.listavip.repositorys;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.alura.listavip.models.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long>{
+
+}
